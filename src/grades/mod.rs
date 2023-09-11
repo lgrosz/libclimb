a19f29924eeff5ceeaa7383e31a29565e484eeae
Logan Grosz <logan.grosz@gmail.com>
2023-09-10 19:54:34 -0600

    Adds Fontainebleau grades

---
+++ src/grades/mod.rs
@@ -0,0 +1,6 @@
+//! Insight to climbing route difficulty.
+//!
+//! Climb route difficulty is specified through various grading systems. This module allows for use
+//! of various grading systems.
+
+pub mod fontainebleau;
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+#![warn(missing_docs)]
+
+//! A crate a generic rock climbing library
+//!
+//! This crate exposes data types for use in rock climbing related applications.
+
+pub mod grades;
