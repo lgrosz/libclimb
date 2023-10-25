240cb9c26c1e55c10681adbb1e205fd4702ef68b
Logan Grosz <logan.grosz@gmail.com>
2023-10-24 22:04:11 -0600

    Prefix Fontainebleau display string with "F"
    The Fontainebleau bouldering grade is generally prefixed with "F" to
    differentiate it with the French route grades (which are generally
    prefixed with "f").

--- src/grades/fontainebleau/grade.rs
+++ src/grades/fontainebleau/grade.rs
@@ -87,7 +87,7 @@ impl fmt::Display for Division {
 
 impl fmt::Display for Fontainebleau {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.level)?;
+        write!(f, "F{}", self.level)?;
 
         match &self.division {
             Some(division) => write!(f, "{}", division),
